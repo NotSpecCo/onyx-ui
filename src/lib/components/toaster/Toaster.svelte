62a233c15a225a54ab4e553935b4a8e731296078
Garrett Downs <garredow@outlook.com>
2022-04-23 10:32:43 -0400

    feat(toaster): add message types

--- src/lib/components/toaster/Toaster.svelte
+++ src/lib/components/toaster/Toaster.svelte
@@ -1,4 +1,8 @@
 <script lang="ts">
+  import MdCheck from 'svelte-icons/md/MdCheck.svelte';
+  import MdErrorOutline from 'svelte-icons/md/MdErrorOutline.svelte';
+  import MdInfoOutline from 'svelte-icons/md/MdInfoOutline.svelte';
+  import MdWarning from 'svelte-icons/md/MdWarning.svelte';
   import { IconSize, RenderState } from '../../enums';
   import { settings, toaster } from '../../stores';
   import Icon from '../icon/Icon.svelte';
@@ -13,15 +17,40 @@
   {#if $settings.toasterLocation === 'bottom'}
     <div class="header" />
   {/if}
-  <div class="toast">
-    {#if $toaster.data.icon}
+  <div
+    class="toast"
+    class:warning={$toaster.data.type === 'warning'}
+    class:error={$toaster.data.type === 'error'}
+    class:success={$toaster.data.type === 'success'}
+  >
+    {#if $toaster.data.type === 'info'}
       <div class="icon">
         <Icon size={IconSize.Small} color="var(--app-text-color)">
-          <svelte:component this={$toaster.data.icon} />
+          <svelte:component this={$toaster.data.icon || MdInfoOutline} />
+        </Icon>
+      </div>
+    {:else if $toaster.data.type === 'warning'}
+      <div class="icon">
+        <Icon size={IconSize.Small} color="var(--warning-color)">
+          <svelte:component this={$toaster.data.icon || MdWarning} />
+        </Icon>
+      </div>
+    {:else if $toaster.data.type === 'error'}
+      <div class="icon">
+        <Icon size={IconSize.Small} color="var(--error-color)">
+          <svelte:component this={$toaster.data.icon || MdErrorOutline} />
+        </Icon>
+      </div>
+    {:else if $toaster.data.type === 'success'}
+      <div class="icon">
+        <Icon size={IconSize.Small} color="var(--success-color)">
+          <svelte:component this={$toaster.data.icon || MdCheck} />
         </Icon>
       </div>
     {/if}
-    {$toaster.data?.title}
+    <div class="text">
+      {$toaster.data?.title}
+    </div>
   </div>
   {#if $settings.toasterLocation === 'top'}
     <div class="footer" />
@@ -67,11 +96,20 @@
   .toast {
     background-color: var(--app-bg-color);
     color: var(--app-text-color);
-    font-weight: var(--bold-font-weight);
-    padding: 3px 5px;
+    font-weight: 600;
+    padding: 5px;
     display: flex;
     align-items: center;
     z-index: 1;
+  }
+  .toast.warning {
+    color: var(--warning-color);
+  }
+  .toast.error {
+    color: var(--error-color);
+  }
+  .toast.success {
+    color: var(--success-color);
   }
   .icon {
     margin-right: 5px;
--- src/lib/models/Toast.ts
+++ src/lib/models/Toast.ts
@@ -2,5 +2,6 @@ import type { SvelteComponent } from 'svelte';
 
 export type Toast = {
   icon?: typeof SvelteComponent;
+  type: 'info' | 'warning' | 'error' | 'success';
   title: string;
 };
